c372f881118e024ce6c3c816002b1f68d06c221d
Joel Eisner <jeisner93@gmail.com>
2023-12-03 14:30:03 -0500

    Math library addition: In range
    Adds a new `inRange(value: number, [min, max]: [min: number, max:
    number], offset?: number)` function to check if a given number is within
    a given range and optional offset. This will be useful for grid
    operations, such as day 3.

--- lib/math.ts
+++ lib/math.ts
@@ -29,3 +29,11 @@ export function divide(first: number, second: number) {
 export function divideAll(numbers: number[]) {
 	return numbers.reduce(divide);
 }
+
+export function inRange(
+	value: number,
+	[min, max]: [min: number, max: number],
+	offset = 0
+) {
+	return (value - (min - offset)) * (value - (max + offset)) <= 0;
+}
