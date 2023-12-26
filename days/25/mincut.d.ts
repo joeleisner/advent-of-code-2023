fc5d0cb49652e1ebd5f1290b61f4173c746a9ec0
Joel Eisner <jeisner93@gmail.com>
2023-12-26 13:01:19 -0500

    Mincut type definition changes
    Updated the `mincut.d.ts` type definitions under day 25 to make its
    generic type match the language used by the package itself, as well as
    making it an explicit 2-element tuple.

--- days/25/mincut.d.ts
+++ days/25/mincut.d.ts
@@ -1,5 +1,5 @@
 declare module '@graph-algorithm/minimum-cut' {
-	export function mincut<TConnection>(
-		connections: TConnection[]
-	): Generator<TConnection>;
+	export function mincut<TEdge extends [any, any]>(
+		edges: TEdge[]
+	): Generator<TEdge>;
 }
